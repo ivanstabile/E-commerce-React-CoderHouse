7ba9f89701afbec1995929f4c43083f5a5358eb4
ivan stabile <ivan_stabile@hotmail.com>
2021-08-14 10:16:51 -0300

    componente con saludo

--- src/components/container/ItemListComponent.js
+++ src/components/container/ItemListComponent.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./ItemListComponent.scss";
-export const ItemListComponent = () => {
+export const ItemListComponent = ({ greeting }) => {
     return (
         <div className="itemListComponent">
-            <h2>Hola mundo!</h2>
+            <h2>{greeting}</h2>
         </div>
     );
 };
